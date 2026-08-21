0b3ef518a930a32571eb22f282fedb5695d8f8dd
Federico Fusco <40967674+federicofusco@users.noreply.github.com>
2026-08-21 13:07:16 +0200

    Rust refactor

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Checks that the correct number of arguments has been passed
-if [[ $# -ne 0 ]] 
-then
-	gcc src/stack.c src/main.c src/compiler.c src/log.c src/debug.c src/object.c -lm -o $1 -O5
-else
-	echo "Usage: ./build.sh [OUTPUT FILE]"
-fi
